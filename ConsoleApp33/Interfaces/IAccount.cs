f112104089c01baee697377abb98e6fc83291b76
hajimat999 <hajimatfa@code.edu.az>
2022-04-19 22:31:23 +0400

    Megan Thee

---
+++ ConsoleApp33/Interfaces/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp33.Interfaces
+{
+    interface IAccount
+    {
+        public bool PasswordChecker(string password);
+        public void ShowInfo();
+        
+
+        
+    }
+}
---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ConsoleApp33.Interfaces;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        private static object password;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Password Daxil edin");
+            
+            string password = Console.ReadLine();
+            string email = "Hajimat";
+            User user = new User(email, password);
+           
+            
+            user.FullName = "Hajimat Alisultanov";
+            user.ShowInfo();
+
+
+            
+        }
+    }
+}
